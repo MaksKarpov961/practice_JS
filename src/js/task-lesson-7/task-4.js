e054071b86111a3b3ed371cb5b3905ba159f4e21
MaksKarpov <ozuzuandvduvatel@gmail.com>
2024-09-26 10:57:22 +0300

    task 4

--- src/js/task-lesson-7/task-4.js
+++ src/js/task-lesson-7/task-4.js
@@ -1,1 +1,26 @@
 
+const loginForm  = document.querySelector('.login-form')
+
+loginForm .addEventListener('submit', eventLisenerSubmit)
+
+function eventLisenerSubmit(event) {
+  event.preventDefault();// Зупиняємо відправку форми
+  
+  const email = loginForm.elements.email.value.trim()
+  const password = loginForm.elements.password.value.trim()
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in')
+    return
+  }
+  
+  const userLoginData = { email: email, password:password,}
+  
+  console.log(userLoginData);
+  
+  loginForm.reset();
+
+  // loginForm.elements.email.value = '' ;
+  // loginForm.elements.password.value = '' ;
+
+}
