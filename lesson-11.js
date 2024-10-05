14fe0eaa56b5ec616826ff13949bc250b301adf5
MaksKarpov961 <ozuzuandvduvatel@gmail.com>
2024-10-05 13:25:33 +0000

    Deploying to gh-pages from @ MaksKarpov961/practice_JS@85c5dca77bea1a40a63e3dca114c4ade273ee584 🚀

--- lesson-11.js
+++ lesson-11.js
@@ -1,9 +1,9 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as g}from"./assets/vendor-DbVqBAlR.js";function h(t){const s="https://pixabay.com/api/?",a="45713433-433c1b648e48abad27090f3cc",r=new URLSearchParams({key:a,q:t,image_type:"photo",orientation:"horizontal",safesearch:!1,per_page:"20"});return fetch(`${s}${r}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>e.hits).catch(e=>{console.log("Помилка при завантаженні фото:",e)})}const l={searchForm:document.querySelector(".search-form"),startSearchBtn:document.querySelector(".js-start-search"),galleryEl:document.querySelector(".gallery")};function p(t){return t.map(a=>{const{webformatURL:r,largeImageURL:e,tags:n,likes:o,views:c,comments:i,downloads:m}=a;return`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as u}from"./assets/vendor-DbVqBAlR.js";function g(r){const l="https://pixabay.com/api/?",a="45713433-433c1b648e48abad27090f3cc",s=new URLSearchParams({key:a,q:r,image_type:"photo",orientation:"horizontal",safesearch:!1,per_page:"20"});return fetch(`${l}${s}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>e.hits).catch(e=>{console.log("Помилка при завантаженні фото:",e)})}const t={searchForm:document.querySelector(".search-form"),startSearchBtn:document.querySelector(".js-start-search"),galleryEl:document.querySelector(".gallery"),loader:document.querySelector(".loader")};function h(r){return r.map(a=>{const{webformatURL:s,largeImageURL:e,tags:n,likes:o,views:c,comments:i,downloads:m}=a;return`
             <li class="gallery-item">
               <a class="gallery-link" href="${e}">
                 <img
                   class="gallery-image"
-                  src="${r}"
+                  src="${s}"
                   alt="${n}"
                   loading="lazy"
               /></a>
@@ -14,5 +14,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
                 <li class="img-content-descr">Downloads<span>${m}</span></li>
               </ul>
             </li>
-      `}).join("")}l.searchForm.addEventListener("submit",u);function u(t){t.preventDefault(),l.galleryEl.innerHTML="";const s=t.target.search.value;h(s).then(a=>{const r=p(a);l.galleryEl.insertAdjacentHTML("afterbegin",r);const e=new g(".gallery a");e.on("show.simplelightbox",function(){e.refresh()})}).catch(a=>{console.log("Помилка при завантаженні фото:",a)})}
+      `}).join("")}t.searchForm.addEventListener("submit",d);function d(r){r.preventDefault(),t.galleryEl.innerHTML="";const l=r.target.search.value;g(l).then(a=>{t.loader.classList.add("visually-hidden");const s=h(a);t.galleryEl.insertAdjacentHTML("afterbegin",s);const e=new u(".gallery a");e.on("show.simplelightbox",function(){e.refresh()})}).catch(a=>{console.log("Помилка при завантаженні фото:",a)}).finally(t.loader.classList.remove("visually-hidden"))}
 //# sourceMappingURL=lesson-11.js.map
