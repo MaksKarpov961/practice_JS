be16cb2b1bbe2321f9244f4a5c94de1a392c8e52
MaksKarpov <ozuzuandvduvatel@gmail.com>
2024-10-06 15:05:08 +0300

    End lesson 11

--- src/js/lesson-11-main.js
+++ src/js/lesson-11-main.js
@@ -1,37 +1,61 @@
-// Описаний у документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 import { getPhotos } from "./lessons-11/pixabay-api";
 import { refsLes11 } from "./lessons-11/refs-les11";
 import { createMarkupGallery } from "./lessons-11/render-function";
 
-
 refsLes11.searchForm.addEventListener('submit', sumbitFormSearch)
 
 function sumbitFormSearch(event) {
-  event.preventDefault()
+  event.preventDefault();
   
-  refsLes11.galleryEl.innerHTML=''
+  refsLes11.galleryEl.innerHTML = '';
   const query = event.target.search.value;
-  
+
+  refsLes11.loader.classList.remove('visually-hidden');
+
   getPhotos(query)
     .then(photos => {
-      refsLes11.loader.classList.add('visually-hidden')
+      if (photos.length === 0) {
+        iziToast.show({
+          title: '❌', 
+          message: `Sorry, there are no images matching your search "${query}". Please try again!`,
+          color: 'red',
+          backgroundColor: '#ffcccc',
+          position: 'topRight',
+          timeout: 5000,
+          close: true,
+          pauseOnHover: true,
+          progressBar: true,
+        });
+        return;
+      }
+
       const markup = createMarkupGallery(photos);
       refsLes11.galleryEl.insertAdjacentHTML('afterbegin', markup);
 
-      const  gallery = new SimpleLightbox('.gallery a');
+      const gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
       gallery.on('show.simplelightbox', function () {
-        gallery.refresh()
-});
+        gallery.refresh();
+      });
     })
     .catch(error => {
       console.log('Помилка при завантаженні фото:', error);
-    }).finally(
-      refsLes11.loader.classList.remove('visually-hidden')
-    );
+      iziToast.show({
+        title: 'Помилка',
+        message: 'Щось пішло не так, спробуйте ще раз пізніше.',
+        color: 'red',
+        position: 'topRight',
+        timeout: 5000,
+      });
+    })
+    .finally(() => {
+      refsLes11.loader.classList.add('visually-hidden');
+    });
 }
-
